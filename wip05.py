608cda79a852a7835e2a2f892dfb3a57e6af605e
mzalota <807e3614d030487ae307d26e66a20d875a40a8ce>
2019-01-26 12:42:15 +0100

    refactored Feature Recognission function

--- wip05.py
+++ wip05.py
@@ -10,33 +10,35 @@
 # template matching 
 import cv2 
 import numpy as np 
-  
-# Read the main image 
-img_rgb = cv2.imread('C:/workspaces/AnjutkaVideo/frames/frame7.jpg')
-  
-# Convert it to grayscale 
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
-  
-# Read the template 
-template = cv2.imread('C:/workspaces/AnjutkaVideo/crab_from_frame_1.png',0) 
-  
-# Store width and heigth of template in w and h 
-w, h = template.shape[::-1] 
-  
-# Perform match operations. 
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED) 
-  
-# Specify a threshold (it was 0.8)
-threshold = 0.6
-  
-# Store the coordinates of matched area in a numpy array 
-loc = np.where( res >= threshold)  
-  
-# Draw a rectangle around the matched region. 
-for pt in zip(*loc[::-1]): 
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2) 
-  
-# Show the final image with the matched area. 
+
+
+def highlightMatchedFeature(img_rgb, template):
+    # Convert it to grayscale
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    # Read the template
+    # Store width and heigth of template in w and h
+    w, h = template.shape[::-1]
+    # Perform match operations.
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    # Specify a threshold (it was 0.8)
+    threshold = 0.6
+    # Store the coordinates of matched area in a numpy array
+    loc = np.where(res >= threshold)
+    # Draw a rectangle around the matched region.
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+
+    # Show the final image with the matched area.
+
+
+# Read the main image
+imagePath = 'C:/workspaces/AnjutkaVideo/frames/frame7.jpg'
+feature_image = 'C:/workspaces/AnjutkaVideo/crab_from_frame_1.png'
+
+img_rgb = cv2.imread(imagePath)
+template = cv2.imread(feature_image, 0)
+
+highlightMatchedFeature(img_rgb, template)
 cv2.imshow('Detected',img_rgb) 
 
 cv2.waitKey(0)
