47afdcede57c06819dd50298277fa66bf6c2fb76
mzalota <807e3614d030487ae307d26e66a20d875a40a8ce>
2019-08-04 22:31:58 +0200

    I think I got it back to working... finding index for a maximum gap was tricky

--- manuallyImproveRedDots.py
+++ manuallyImproveRedDots.py
@@ -12,16 +12,16 @@ from lib.common import Point, Box
 from datetime import datetime
 
 
-rootDir ="C:/workspaces/AnjutkaVideo/Kara_Sea_Crab_Video_st_5993_2018/"
+#rootDir ="C:/workspaces/AnjutkaVideo/Kara_Sea_Crab_Video_st_5993_2018/"
 #videoFileName = "V3__R_20180915_205551"
-videoFileName = "V4__R_20180915_210447"
+#videoFileName = "V4__R_20180915_210447"
 #videoFileName = "V5__R_20180915_211343"
 #videoFileName = "V6__R_20180915_212238"
 
-#rootDir = "C:/workspaces/AnjutkaVideo/KaraSeaCrabVideoBlagopoluchiyaBay2018/"
+rootDir = "C:/workspaces/AnjutkaVideo/KaraSeaCrabVideoBlagopoluchiyaBay2018/"
 #videoFileName = "V1_R_20180911_165259"
 #videoFileName = "V2_R_20180911_165730"
-#videoFileName = "V3_R_20180911_170159"
+videoFileName = "V3_R_20180911_170159"
 
 folderStruct = FolderStructure(rootDir, videoFileName)
 #StreamToLogger(folderStruct.getLogFilepath())
