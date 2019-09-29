abaaea3d6456f82712c5f8ceb1d728bb7b674d69
mzalota <807e3614d030487ae307d26e66a20d875a40a8ce>
2019-09-29 21:30:42 +0200

    refactored SeeFloor::jumpToSeefloorSlice and its private functions

--- lib/SeeFloor.py
+++ lib/SeeFloor.py
@@ -20,32 +20,88 @@ class SeeFloor:
         minFrameID = self.__badFramesData.firstGoodFrameAfter(minFrameID)
         return minFrameID
 
-    def jumpToSeefloorSlice(self, frame_id, frames_to_jump = 1):
-        return self.jump_to_next_seefloor_slice(frame_id,frames_to_jump)
+    def jumpToSeefloorSlice(self, frame_id, frames_to_jump):
+        if frame_id < self.__driftData.minFrameID():
+            return int(self.__driftData.minFrameID())
 
-    def jump_to_previous_seefloor_slice(self, frame_id, frames_to_jump = 1):
-        if self.__badFramesData.is_bad_frame(frame_id):
-            new_frame_id = self.__badFramesData.firstGoodFrameBefore(frame_id)
-            if new_frame_id < self.__driftData.minFrameID():
-                new_frame_id = self.__driftData.minFrameID()
-            return new_frame_id
-        else:
-            pixels_to_jump = -FramesStitcher.FRAME_HEIGHT*frames_to_jump
-            new_frame_id =  int(self.__driftData.getNextFrame(pixels_to_jump, frame_id))
-            return new_frame_id
+        if frame_id > self.__driftData.maxFrameID():
+            return int(self.__driftData.maxFrameID())
 
-    def jump_to_next_seefloor_slice(self, frame_id, frames_to_jump = 1):
-        if self.__badFramesData.is_bad_frame(frame_id):
-            new_frame_id = self.__badFramesData.firstGoodFrameAfter(frame_id)
-            if new_frame_id > self.__driftData.maxFrameID():
-                new_frame_id = self.__driftData.maxFrameID()
-            return new_frame_id
-
-        pixels_to_jump = FramesStitcher.FRAME_HEIGHT*frames_to_jump
-        new_frame_id = int(self.__driftData.getNextFrame(pixels_to_jump, frame_id))
-
-
-        print ("new_frame_id", new_frame_id)
-        new_frame_id = self.jump_to_previous_seefloor_slice(new_frame_id,1)
+        new_frame_id = frame_id
+        while frames_to_jump != 0:
+            if frames_to_jump > 0:
+                new_frame_id = int(self.__jump_to_next_seefloor_slice(new_frame_id))
+                frames_to_jump = frames_to_jump-1
+            if frames_to_jump < 0:
+                new_frame_id = int(self.__jump_to_previous_seefloor_slice(new_frame_id))
+                frames_to_jump = frames_to_jump+1
 
         return new_frame_id
+
+    def __adjust_outofbound_values(self,frame_id):
+        if frame_id < self.__driftData.minFrameID():
+            return self.__driftData.minFrameID()
+
+        if frame_id > self.__driftData.maxFrameID():
+            return self.__driftData.maxFrameID()
+
+        return frame_id
+
+
+    def __jump_to_previous_seefloor_slice(self, frame_id):
+        if frame_id < self.__driftData.minFrameID():
+            return self.__driftData.minFrameID()
+
+        if frame_id > self.__driftData.maxFrameID():
+            return self.__driftData.maxFrameID()
+
+        if self.__badFramesData.is_bad_frame(frame_id):
+            # we are currently in a bad frame. Jump out of this bad segment to the closest previous good frame
+            new_frame_id = self.__badFramesData.firstGoodFrameBefore(frame_id)
+            return self.__adjust_outofbound_values(new_frame_id)
+
+        first_good_frame = self.__badFramesData.firstBadFrameBefore(frame_id) + 1
+        if frame_id == first_good_frame:
+            #frame_id is the first frame in this good segment. Jump over the previous bad segment
+            return self.__jump_to_previous_seefloor_slice(frame_id - 1)
+
+        # we are in a good segment and not in its first frame.
+        pixels_to_jump = FramesStitcher.FRAME_HEIGHT * (-1)
+        new_frame_id = int(self.__driftData.getNextFrame(pixels_to_jump, frame_id))
+
+        if (first_good_frame >= new_frame_id):
+            #the current good segment does not enough runway from frame_id to jump FramesStitcher.FRAME_HEIGHT pixels back.
+            #so jump to the first good frame in this segment
+            return first_good_frame
+        else:
+            return new_frame_id
+
+
+    def __jump_to_next_seefloor_slice(self, frame_id):
+        if frame_id < self.__driftData.minFrameID():
+            return self.__driftData.minFrameID()
+
+        if frame_id > self.__driftData.maxFrameID():
+            return self.__driftData.maxFrameID()
+
+        if self.__badFramesData.is_bad_frame(frame_id):
+            #we are currently in a bad frame. Jump out of this bad segment to the closest next good frame
+            new_frame_id = self.__badFramesData.firstGoodFrameAfter(frame_id)
+            return self.__adjust_outofbound_values(new_frame_id)
+
+        last_good_frame = self.__badFramesData.firstBadFrameAfter(frame_id) - 1
+        if frame_id == last_good_frame:
+            #Frame_id is the last good frame before a bad segment. Jump over this bad segment to the first next good frame
+            return self.__jump_to_next_seefloor_slice(frame_id + 1)
+
+        # we are in a good segment and not in its last frame.
+        pixels_to_jump = FramesStitcher.FRAME_HEIGHT
+        new_frame_id = int(self.__driftData.getNextFrame(pixels_to_jump, frame_id))
+
+        if (last_good_frame < new_frame_id):
+            #the current good segment does not enough runway from frame_id to jump FramesStitcher.FRAME_HEIGHT pixels.
+            #so jump to the last good frame in this segment
+            return last_good_frame
+        else:
+            return new_frame_id
+
--- tests/test_seeFloor.py
+++ tests/test_seeFloor.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from lib.BadFramesData import BadFramesData
 from lib.DriftData import DriftData
+from lib.FramesStitcher import FramesStitcher
 from lib.SeeFloor import SeeFloor
 
 
@@ -55,6 +56,7 @@ class TestSeeFloor(TestCase):
     def test_jumpToSeefloorSlice(self):
         # Setup
         drifts_df = pd.DataFrame()
+        drifts_df = drifts_df.append({'frameNumber': int(93), 'driftY': 0, 'driftX': 0}, ignore_index=True)
         drifts_df = drifts_df.append({'frameNumber': int(95), 'driftY': 16, 'driftX': 0}, ignore_index=True)
         drifts_df = drifts_df.append({'frameNumber': int(100), 'driftY': 16, 'driftX': 0}, ignore_index=True)
         drifts_df = drifts_df.append({'frameNumber': int(102), 'driftY': 16, 'driftX': 0}, ignore_index=True)
@@ -69,7 +71,7 @@ class TestSeeFloor(TestCase):
         badframes_df = badframes_df.append({BadFramesData.COLNAME_startfFrameNumber : int(92), BadFramesData.COLNAME_endFrameNumber: 95}, ignore_index=True)
         badframesData = BadFramesData.createFromDataFrame(None, badframes_df)
 
-        #--- good frames are 96,97,98 and 102,103. startFrame is 95, endFrame is 107
+        #--- good frames are 96,97,98 and 102,103. startFrame is 93, endFrame is 107
 
         seeFloor = SeeFloor(driftData, badframesData, None)
 
@@ -77,7 +79,235 @@ class TestSeeFloor(TestCase):
 
 
         # Assert
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(-333,1)) # if parameter is out of bounds show first/last frame
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(91,1))
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(92,1))
+        self.assertEqual(96, seeFloor.jumpToSeefloorSlice(93,1))
+        self.assertEqual(96, seeFloor.jumpToSeefloorSlice(94,1))
         self.assertEqual(96, seeFloor.jumpToSeefloorSlice(95,1))
         self.assertEqual(98, seeFloor.jumpToSeefloorSlice(96,1))
         self.assertEqual(98, seeFloor.jumpToSeefloorSlice(97,1))
         self.assertEqual(102, seeFloor.jumpToSeefloorSlice(98,1))
+        self.assertEqual(102, seeFloor.jumpToSeefloorSlice(99,1))
+        self.assertEqual(102, seeFloor.jumpToSeefloorSlice(100,1))
+        self.assertEqual(102, seeFloor.jumpToSeefloorSlice(101,1))
+        self.assertEqual(103, seeFloor.jumpToSeefloorSlice(102,1))
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(103,1))
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(104,1))
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(105,1))
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(106,1))
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(107,1))
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(108,1))
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(2222,1)) # if parameter is out of bounds show first/last frame
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(109, 1))
+
+
+
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(-333,-1)) # if parameter is out of bounds show first/last frame
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(91,-1)) #<-- out-of-bound frame
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(92,-1)) #<-- out-of-bound frame
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(93,-1)) #<-- 93 is bad frame - its FIRST frame
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(94,-1)) #<-- 94 is bad frame
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(95,-1)) #<-- 95 is bad frame
+        self.assertEqual(93, seeFloor.jumpToSeefloorSlice(96,-1))
+        self.assertEqual(96, seeFloor.jumpToSeefloorSlice(97,-1))
+        self.assertEqual(96, seeFloor.jumpToSeefloorSlice(98,-1))
+        self.assertEqual(98, seeFloor.jumpToSeefloorSlice(99,-1)) #<-- 99 is bad frame
+        self.assertEqual(98, seeFloor.jumpToSeefloorSlice(100,-1)) #<-- 100 is bad frame
+        self.assertEqual(98, seeFloor.jumpToSeefloorSlice(101,-1)) #<-- 101 is bad frame
+        self.assertEqual(98, seeFloor.jumpToSeefloorSlice(102,-1))
+        self.assertEqual(102, seeFloor.jumpToSeefloorSlice(103,-1))
+        self.assertEqual(103, seeFloor.jumpToSeefloorSlice(104,-1)) #<-- 104 is bad frame
+        self.assertEqual(103, seeFloor.jumpToSeefloorSlice(105,-1)) #<-- 105 is bad frame
+        self.assertEqual(103, seeFloor.jumpToSeefloorSlice(106,-1)) #<-- 106 is bad frame
+        self.assertEqual(103, seeFloor.jumpToSeefloorSlice(107,-1)) #<-- 107 is bad frame - its LAST frame
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(108,-1)) #<-- out-of-bound frame
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(2222,-1)) #<-- out-of-bound frame
+        self.assertEqual(107, seeFloor.jumpToSeefloorSlice(109, -1)) #<-- out-of-bound frame
+
+
+    def test_jumpToSeefloorSlice_multiSlice(self):
+        # Setup
+        pixels_in_frame = FramesStitcher.FRAME_HEIGHT
+        pixels_in_half_frame = int(pixels_in_frame/2)
+
+        drifts_df = pd.DataFrame()
+        drifts_df = drifts_df.append({'frameNumber': int(100), 'driftY': 0, 'driftX': 0}, ignore_index=True)
+        drifts_df = drifts_df.append({'frameNumber': int(200), 'driftY': pixels_in_half_frame, 'driftX': 0}, ignore_index=True)
+        drifts_df = drifts_df.append({'frameNumber': int(300), 'driftY': pixels_in_half_frame, 'driftX': 0}, ignore_index=True)
+        drifts_df = drifts_df.append({'frameNumber': int(400), 'driftY': pixels_in_half_frame, 'driftX': 0}, ignore_index=True)
+        drifts_df = drifts_df.append({'frameNumber': int(500), 'driftY': pixels_in_half_frame, 'driftX': 0}, ignore_index=True)
+        drifts_df = drifts_df.append({'frameNumber': int(600), 'driftY': pixels_in_half_frame, 'driftX': 0}, ignore_index=True)
+        drifts_df = drifts_df.append({'frameNumber': int(700), 'driftY': pixels_in_half_frame, 'driftX': 0}, ignore_index=True)
+        driftData = DriftData(drifts_df)
+
+        badframes_df = pd.DataFrame()
+        badframes_df = badframes_df.append({BadFramesData.COLNAME_startfFrameNumber : int(90), BadFramesData.COLNAME_endFrameNumber: 149}, ignore_index=True)
+        badframes_df = badframes_df.append({BadFramesData.COLNAME_startfFrameNumber : int(575), BadFramesData.COLNAME_endFrameNumber: 624}, ignore_index=True)
+        badframes_df = badframes_df.append({BadFramesData.COLNAME_startfFrameNumber : int(690), BadFramesData.COLNAME_endFrameNumber: 790}, ignore_index=True)
+        badframesData = BadFramesData.createFromDataFrame(None, badframes_df)
+
+        #--- good frames are 150-574 and 625-689. startFrame is 100, endFrame is 700
+
+        seeFloor = SeeFloor(driftData, badframesData, None)
+
+        # Exercise
+        # Assert
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(10,1)) # if parameter is out of bounds show first/last frame
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(99,1))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(100,1))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(101,1))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(149,1))
+        self.assertEqual(350, seeFloor.jumpToSeefloorSlice(150,1))
+        self.assertEqual(352, seeFloor.jumpToSeefloorSlice(151,1)) #<>>>
+        self.assertEqual(353, seeFloor.jumpToSeefloorSlice(152,1)) #<>>>
+        self.assertEqual(375, seeFloor.jumpToSeefloorSlice(175,1))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(572,1))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(573,1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(574,1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(575,1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(623,1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(624,1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(625,1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(626,1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(627,1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(687,1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(688,1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(689,1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(690,1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(691,1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(699,1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(700,1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(701,1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(702,1))
+
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(10, 2))  # if parameter is out of bounds show first/last frame
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(99, 2))
+
+        # step 1 is to jump over bad block, step 2 is to jump 200 frames
+        self.assertEqual(350, seeFloor.jumpToSeefloorSlice(100, 2))
+        self.assertEqual(350, seeFloor.jumpToSeefloorSlice(101, 2))
+        self.assertEqual(350, seeFloor.jumpToSeefloorSlice(149, 2))
+
+        # normal 2 jumps without any bad blocks in the middle
+        self.assertEqual(550, seeFloor.jumpToSeefloorSlice(150, 2))
+        self.assertEqual(553, seeFloor.jumpToSeefloorSlice(151, 2))
+        self.assertEqual(554, seeFloor.jumpToSeefloorSlice(152, 2))
+        self.assertEqual(555, seeFloor.jumpToSeefloorSlice(153, 2))
+        self.assertEqual(555, seeFloor.jumpToSeefloorSlice(154, 2))  #<-- ?? why diff input but same output. Who is rounding where?
+        self.assertEqual(555, seeFloor.jumpToSeefloorSlice(155, 2))  #<-- ?? why diff input but same output. Who is rounding where?
+        self.assertEqual(558, seeFloor.jumpToSeefloorSlice(156, 2))
+        self.assertEqual(559, seeFloor.jumpToSeefloorSlice(157, 2))
+        self.assertEqual(560, seeFloor.jumpToSeefloorSlice(158, 2))
+        self.assertEqual(560, seeFloor.jumpToSeefloorSlice(159, 2)) #<-- ?? why diff input but same output. Who is rounding where?
+        self.assertEqual(560, seeFloor.jumpToSeefloorSlice(160, 2)) #<-- ?? why diff input but same output. Who is rounding where?
+
+        # jump #1 is 200 frames, then second jump is shorter to the last good frame before bad block
+
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(172,2))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(173,2))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(174,2)) #<-- ??
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(175,2)) #<-- ??
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(176,2))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(177,2))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(178,2))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(179,2))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(180, 2))
+
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(180,3))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(180,4))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(180,5))
+        #self.assertEqual(35444, seeFloor.jumpToSeefloorSlice(150,1))
+
+
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(10,-1)) # if parameter is out of bounds show first/last frame
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(99,-1))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(100,-1))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(101,-1))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(149,-1))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(150,-1))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(151,-1))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(175,-1))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(348,-1))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(349,-1))
+        self.assertEqual(151, seeFloor.jumpToSeefloorSlice(350,-1))
+        self.assertEqual(152, seeFloor.jumpToSeefloorSlice(351,-1))
+        self.assertEqual(374, seeFloor.jumpToSeefloorSlice(573,-1))
+        self.assertEqual(375, seeFloor.jumpToSeefloorSlice(574,-1)) #<--- ??
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(575,-1))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(576,-1))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(623,-1))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(624,-1))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(625,-1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(626,-1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(627,-1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(687,-1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(688,-1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(689,-1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(690,-1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(691,-1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(699,-1))
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(700,-1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(701,-1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(702,-1))
+
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(10, -2))  # if parameter is out of bounds show first/last frame
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(99, -2))
+
+        # step 1 is to jump over bad block, step 2 is to jump 200 frames
+        self.assertEqual(375, seeFloor.jumpToSeefloorSlice(600, -2))
+        self.assertEqual(375, seeFloor.jumpToSeefloorSlice(575, -2))
+
+        #TODO: why 176 and not 175? where did extra 1 come from
+        self.assertEqual(176, seeFloor.jumpToSeefloorSlice(574, -2)) #<--- why 176 and not 175? where did extra 1 come from
+        self.assertEqual(175, seeFloor.jumpToSeefloorSlice(573, -2))
+
+        #TODO:fix test cases below
+        # normal 2 jumps without any bad blocks in the middle
+        self.assertEqual(152, seeFloor.jumpToSeefloorSlice(550, -2))
+        self.assertEqual(151, seeFloor.jumpToSeefloorSlice(549, -2))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(548, -2))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(547, -2))
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(546, -2))  #<-- ?? why diff input but same output. Who is rounding where?
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(545, -2))  #<-- ?? why diff input but same output. Who is rounding where?
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(351, -2))  #<-- ?? why diff input but same output. Who is rounding where?
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(350, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(349, -2)) #<-- ?? why diff input but same output. Who is rounding where?
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(348, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(348, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(152, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(151, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(150, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(149, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(148, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(147, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(103, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(102, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(101, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(100, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(99, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(98, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(92, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(91, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(90, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(89, -2))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(88, -2))
+
+        # jump #1 is 200 frames, then second jump is shorter to the last good frame before bad block
+
+        self.assertEqual(689, seeFloor.jumpToSeefloorSlice(700, -1))
+        self.assertEqual(625, seeFloor.jumpToSeefloorSlice(700, -2))
+        self.assertEqual(574, seeFloor.jumpToSeefloorSlice(700, -3))
+        self.assertEqual(375, seeFloor.jumpToSeefloorSlice(700, -4))
+        self.assertEqual(176, seeFloor.jumpToSeefloorSlice(700, -5)) #<-- ?? why not 175?
+        self.assertEqual(150, seeFloor.jumpToSeefloorSlice(700, -6))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(700, -7))
+        self.assertEqual(100, seeFloor.jumpToSeefloorSlice(700, -8))
+
+        self.assertEqual(176, seeFloor.jumpToSeefloorSlice(699,-5))
+        self.assertEqual(176, seeFloor.jumpToSeefloorSlice(698,-5))
+
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(701, -1))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(701, -2))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(701, -3))
+        self.assertEqual(700, seeFloor.jumpToSeefloorSlice(702, -3))
